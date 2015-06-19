27957dde3647b983aa701efd09b318e512945465
congsun <william9135@gmail.com>
2015-06-18 20:37:36 -0400

    first commit

---
+++ FISInsta/Model/InstaObject.h
@@ -0,0 +1,13 @@
+//
+//  InstaObject.h
+//  FISInsta
+//
+//  Created by Cong Sun on 6/18/15.
+//  Copyright (c) 2015 Cong Sun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InstaObject : NSObject
+
+@end
---
+++ FISInsta/Model/InstaObject.m
@@ -0,0 +1,13 @@
+//
+//  InstaObject.m
+//  FISInsta
+//
+//  Created by Cong Sun on 6/18/15.
+//  Copyright (c) 2015 Cong Sun. All rights reserved.
+//
+
+#import "InstaObject.h"
+
+@implementation InstaObject
+
+@end
