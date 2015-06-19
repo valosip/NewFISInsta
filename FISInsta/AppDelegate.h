b51215ce2f141e0a60e6187a4963204b62cce18f
congsun <william9135@gmail.com>
2015-06-18 20:21:04 -0400

    Initial Commit

---
+++ FISInsta/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FISInsta
+//
+//  Created by Cong Sun on 6/18/15.
+//  Copyright (c) 2015 Cong Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
